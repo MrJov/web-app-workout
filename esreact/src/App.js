5190463703a89f3b3132ea3febc6e76d0cb57404
David Lorenzi <dlorenzi@deloitte.it>
2021-06-20 10:43:30 +0200

    First site sample

--- esreact/src/App.js
+++ esreact/src/App.js
@@ -1,33 +1,21 @@
-import logo from './logo.svg'
-import './css/App.css'
 import { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import MainTemplate from './components/mainLayout/template/mainTemplate'
+import Home from './components/home/home'
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      isTextVisible: false
-    }
-  }
-  toggleText(){
-    this.setState({isTextVisible: !this.state.isTextVisible})
-  }
-  render() {
-    var textToShow = this.state.isTextVisible ? (<span>Ciao adesso mi vedi!</span>) : (<span></span>)
+  constructor(props){ super(props) }
 
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p> Edit <code>src/App.js</code> and save to reload. </p>
-          <p>Powered by Jov</p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React </a>
-          <button className="btn btn-primary" style={{width: "200px", height: "200px"}} onClick={this.toggleText.bind(this)}>
-            Toggle Text
-          </button>
-          {textToShow}
-        </header>
-      </div>
+      <BrowserRouter>
+        <MainTemplate>
+          <Switch>
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </MainTemplate>
+      </BrowserRouter>
     );
   }
 }
